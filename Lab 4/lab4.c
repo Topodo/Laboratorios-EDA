58b62f99a13c569847901683fda4ed5915aedb5f
Ariel Tirado <cbzn1994@gmail.com>
2016-11-12 23:30:04 -0400

    Agregada la función para particionar nodos
    Se agregó la función encargada de particionar un nodo si es que este no
    cumple con el criterio de uniformidad

--- Lab 4/lab4.c
+++ Lab 4/lab4.c
@@ -4,7 +4,6 @@
 //Definición de estructuras
 typedef struct nodo{
 	int **matriz;
-	int uniformidad;
 	int N;
 	int region;
 	struct nodo *primerHijo;
@@ -18,7 +17,6 @@ nodo *crearNodo(int N){
 	nodo *nuevo = (nodo *)malloc(sizeof(nodo));
 	if(nuevo != NULL){
 		nuevo -> matriz = NULL;
-		nuevo -> uniformidad = 0;
 		nuevo -> N = N;
 		nuevo -> region = 0;
 		nuevo -> primerHijo = NULL;
@@ -60,8 +58,7 @@ int verificarCriterio(int **matriz, int N){
 
 		}
 	}
-	printf("El menor es: %d y el mayor: %d\n", menor, mayor);
-	if(mayor - menor > 10){
+	if(mayor - menor > 2){
 		return 0;
 	}
 	else{
@@ -77,6 +74,71 @@ void mostrar(int **matriz, int N){
 		}
 		printf("\n");
 	}
+}
+
+//Función que particiona un nodo del árbol en 4 partes
+nodo *particionar(nodo *actual){
+	nodo *primerHijo = crearNodo(actual -> N / 2);
+	nodo *segundoHijo = crearNodo(actual -> N / 2);
+	nodo *tercerHijo = crearNodo(actual -> N / 2); 
+	nodo *cuartoHijo = crearNodo(actual -> N / 2);
+	int i, j;
+	//Se asigna memoria para las submatrices
+	int **primeraMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
+	int **segundaMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
+	int  **terceraMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
+	int **cuartaMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
+	for(i = 0; i < actual -> N / 2; i++){
+		primeraMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
+		segundaMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
+	 	terceraMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2); 
+	 	cuartaMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
+	}
+	//Se realiza el proceso de partición de la matriz del nodo
+	for(i = 0; i < actual -> N / 2; i++){
+		for(j = 0; j < actual -> N / 2; j++){
+			primeraMatriz[i][j] = actual -> matriz[i][j];
+		}	
+	}
+	for(i = 0; i < actual -> N / 2; i++){
+		for(j = actual -> N / 2; j < actual -> N; j++){
+			segundaMatriz[i][j - actual -> N / 2] = actual -> matriz[i][j];
+		}
+	}
+	for(i = actual -> N / 2; i < actual -> N; i++){
+		for(j = 0; j < actual -> N / 2; j++){
+			terceraMatriz[i - actual -> N / 2][j] = actual -> matriz[i][j];
+		}
+	}
+	for(i = actual -> N / 2; i < actual -> N; i++){
+		for(j = actual -> N / 2; j < actual -> N; j++){
+			cuartaMatriz[i - actual -> N / 2][j - actual -> N / 2] = actual -> matriz[i][j];
+		}
+	}
+	//Se asignan las submatrices a cada hijo del nodo
+	primerHijo -> matriz = primeraMatriz;
+	segundoHijo -> matriz = segundaMatriz;
+	tercerHijo -> matriz = terceraMatriz;
+	cuartoHijo -> matriz = cuartaMatriz;
+
+	if(primerHijo != NULL && segundoHijo != NULL && tercerHijo != NULL && cuartoHijo != NULL){
+		actual -> primerHijo = primerHijo;
+		actual -> segundoHijo = segundoHijo;
+		actual -> tercerHijo = tercerHijo;
+		actual -> cuartoHijo = cuartoHijo;
+	}
+	return actual;
+}
+
+nodo* preOrden(nodo *raiz){
+	if(raiz != NULL && verificarCriterio(raiz -> matriz, raiz -> N) == 0){
+		raiz = particionar(raiz);
+		preOrden(raiz -> primerHijo);
+		preOrden(raiz -> segundoHijo);
+		preOrden(raiz -> tercerHijo);
+		preOrden(raiz -> cuartoHijo);
+	}
+	return raiz;
 }
 
 //Función que realiza el procedimiento
@@ -96,8 +158,8 @@ void procedimiento(FILE *archivo){
 	}
 	quadtree = crearNodo(N);
 	quadtree -> matriz = matriz;
-	mostrar(quadtree -> matriz, N);
-	i = verificarCriterio(quadtree -> matriz, N);
+	mostrar(quadtree -> matriz, quadtree -> N);
+	preOrden(quadtree);
 }
 
 
