f95d2c85b9621d9683f1de019d23a09e9bf29beb
Topodo <ariel.tirado@usach.cl>
2016-11-16 00:43:34 -0300

    Se agregaron datos para realizar fusión

--- Lab 4/lab4.c
+++ Lab 4/lab4.c
@@ -6,11 +6,18 @@ typedef struct nodo{
 	int **matriz;
 	int N;
 	int region;
+	int uniformidad;
+	int nivel;
+	int esPadre;
+	int idPadre;
+	int mayor;
+	int menor;
 	struct nodo *primerHijo;
 	struct nodo *segundoHijo;
 	struct nodo *tercerHijo;
 	struct nodo *cuartoHijo;
 }nodo;
+
 
 //Función que crea un nodo vacío del árbol
 nodo *crearNodo(int N){
@@ -19,6 +26,12 @@ nodo *crearNodo(int N){
 		nuevo -> matriz = NULL;
 		nuevo -> N = N;
 		nuevo -> region = 0;
+		nuevo -> uniformidad = 0;
+		nuevo -> nivel = 0;
+		nuevo -> esPadre = 0;
+		nuevo -> idPadre = -1;
+		nuevo -> mayor = 0;
+		nuevo -> menor = 0;
 		nuevo -> primerHijo = NULL;
 		nuevo -> segundoHijo = NULL;
 		nuevo -> tercerHijo = NULL;
@@ -33,16 +46,20 @@ nodo *crearNodo(int N){
 
 //Función que crea una matriz de enteros
 int **crearMatriz(int N){
-	int i;
+	int i, j;
 	int **matriz = (int **)malloc(N * sizeof(int *));
 	for(i = 0; i < N; i++){
 		matriz[i] = (int *)malloc(N * sizeof(int));
+		for(j = 0; j < N; j++){
+			matriz[i][j] = 0;
+		}
 	}
 	return matriz;
 }
 
+
 //Función que verifica el criterio de uniformidad
-int verificarCriterio(int **matriz, int N){
+int verificarCriterio(int **matriz, int N, int *uniformidad, int *Mayor, int *Menor){
 	int i, j, menor, mayor;
 	menor = matriz[0][0];
 	mayor = matriz[0][0];
@@ -58,6 +75,9 @@ int verificarCriterio(int **matriz, int N){
 
 		}
 	}
+	*uniformidad = mayor - menor;
+	*Mayor = mayor;
+	*Menor = menor;
 	if(mayor - menor > 2){
 		return 0;
 	}
@@ -84,22 +104,17 @@ nodo *particionar(nodo *actual){
 	nodo *cuartoHijo = crearNodo(actual -> N / 2);
 	int i, j;
 	//Se asigna memoria para las submatrices
-	int **primeraMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
-	int **segundaMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
-	int  **terceraMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
-	int **cuartaMatriz = (int **)malloc(sizeof(int *) * actual -> N / 2);
-	for(i = 0; i < actual -> N / 2; i++){
-		primeraMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
-		segundaMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
-	 	terceraMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2); 
-	 	cuartaMatriz[i] = (int *)malloc(sizeof(int) * actual -> N / 2);
-	}
+	int **primeraMatriz = crearMatriz(actual -> N / 2);
+	int **segundaMatriz = crearMatriz(actual -> N / 2);
+	int  **terceraMatriz = crearMatriz(actual -> N / 2);
+	int **cuartaMatriz = crearMatriz(actual -> N / 2);
+
 	//Se realiza el proceso de partición de la matriz del nodo
 	for(i = 0; i < actual -> N / 2; i++){
 		for(j = 0; j < actual -> N / 2; j++){
 			primeraMatriz[i][j] = actual -> matriz[i][j];
 		}	
-	}
+	}	
 	for(i = 0; i < actual -> N / 2; i++){
 		for(j = actual -> N / 2; j < actual -> N; j++){
 			segundaMatriz[i][j - actual -> N / 2] = actual -> matriz[i][j];
@@ -122,23 +137,64 @@ nodo *particionar(nodo *actual){
 	cuartoHijo -> matriz = cuartaMatriz;
 
 	if(primerHijo != NULL && segundoHijo != NULL && tercerHijo != NULL && cuartoHijo != NULL){
+		//Se asignan los nodos hijos y sus respectivas regiones
 		actual -> primerHijo = primerHijo;
 		actual -> segundoHijo = segundoHijo;
 		actual -> tercerHijo = tercerHijo;
 		actual -> cuartoHijo = cuartoHijo;
+		actual -> primerHijo -> region = actual -> region * 10 + 1;
+		actual -> segundoHijo -> region = actual -> region * 10 + 2;
+		actual -> tercerHijo -> region = actual -> region * 10 + 3;
+		actual -> cuartoHijo -> region = actual -> region * 10 + 4;
 	}
 	return actual;
 }
 
-nodo* preOrden(nodo *raiz){
-	if(raiz != NULL && verificarCriterio(raiz -> matriz, raiz -> N) == 0){
+//Funcion de recorrido en Preorden, que va particionando los nodos y formando el quadtree en funcion del criterio de uniformidad
+nodo* preOrden(nodo *raiz, int nivel, int region, int idPadre){
+	raiz -> nivel = nivel;
+	raiz -> idPadre = idPadre;
+	raiz -> region = region;
+	if(raiz != NULL && verificarCriterio(raiz -> matriz, raiz -> N, &raiz -> uniformidad, &raiz -> mayor, &raiz -> menor) == 0){
+		raiz -> esPadre = 1; //Si se cumple el criterio, el nodo es padre
 		raiz = particionar(raiz);
-		preOrden(raiz -> primerHijo);
-		preOrden(raiz -> segundoHijo);
-		preOrden(raiz -> tercerHijo);
-		preOrden(raiz -> cuartoHijo);
-	}
+		preOrden(raiz -> primerHijo, nivel + 1, raiz -> region * 10 + 1, raiz -> region);
+		preOrden(raiz -> segundoHijo, nivel + 1, raiz -> region * 10 + 2, raiz -> region);
+		preOrden(raiz -> tercerHijo, nivel + 1, raiz -> region * 10 + 3, raiz -> region);
+		preOrden(raiz -> cuartoHijo, nivel + 1, raiz -> region * 10 + 4, raiz -> region);
+	}	
 	return raiz;
+}
+
+void mostrarPreOrden(nodo *raiz){
+	if(raiz != NULL){
+		printf("Nivel: %d, Es padre?: %d, Region: %d, Nodo padre: %d, uniformidad: %d, Mayor: %d, Menor: %d\n", raiz -> nivel, raiz -> esPadre, raiz -> region, raiz -> idPadre, raiz -> uniformidad, raiz -> mayor, raiz -> menor);
+		mostrar(raiz -> matriz, raiz -> N);
+		mostrarPreOrden(raiz -> primerHijo);
+		mostrarPreOrden(raiz -> segundoHijo);
+		mostrarPreOrden(raiz -> tercerHijo);
+		mostrarPreOrden(raiz -> cuartoHijo);
+	}
+}
+void mostrarPostOrden(nodo *raiz){
+	if(raiz != NULL){
+		
+		mostrarPreOrden(raiz -> primerHijo);
+		mostrarPreOrden(raiz -> segundoHijo);
+		mostrarPreOrden(raiz -> tercerHijo);
+		mostrarPreOrden(raiz -> cuartoHijo);
+		printf("Nivel: %d, Es padre?: %d, Region: %d, Nodo padre: %d, uniformidad: %d\n", raiz -> nivel, raiz -> esPadre, raiz -> region, raiz -> idPadre, raiz -> uniformidad);
+		mostrar(raiz -> matriz, raiz -> N);
+	}
+}
+
+
+
+//Función que funsiona las regiones del arbol correspondientes
+void fusionar(nodo *quadtree){
+	//Se crea la matriz que contendrá las regiones respectivas, para luego escribirlas en el archivo de texto
+	int **matriz = crearMatriz(quadtree -> N);
+	FILE *salida = fopen("Salida.out", "w");
 }
 
 //Función que realiza el procedimiento
@@ -158,8 +214,9 @@ void procedimiento(FILE *archivo){
 	}
 	quadtree = crearNodo(N);
 	quadtree -> matriz = matriz;
-	mostrar(quadtree -> matriz, quadtree -> N);
-	preOrden(quadtree);
+	//mostrar(quadtree -> matriz, quadtree -> N);
+	preOrden(quadtree, 0, quadtree -> region, quadtree -> idPadre);
+	mostrarPostOrden(quadtree);
 }
 
 
